34ada896697f9934049d599bdd6b3d24fdb11a4e
Dan Taylor <dan@halltaylors.co.uk>
2026-05-21 22:29:50 +0100

    v70 - global error logging

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
+
+// Global error catcher - log everything before ErrorBoundary swallows it
+window.addEventListener('error', e => {
+  console.error('GLOBAL ERROR:', e.message, e.filename, e.lineno)
+})
+window.addEventListener('unhandledrejection', e => {
+  console.error('UNHANDLED PROMISE:', e.reason)
+})
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ErrorBoundary from './components/ErrorBoundary.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </StrictMode>,
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
 )
